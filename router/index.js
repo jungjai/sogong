f9bec0f7d4ba37b469e5e197d86b9f2c8f6a52bd
jeong <950222@naver.com>
2018-08-25 14:28:43 +0900

    third

--- router/index.js
+++ router/index.js
@@ -3,8 +3,8 @@ var router = express.Router();
 var path = require('path')
 var register = require('./register/index')
 var login = require('./login/index')
-
+var search = require('./search/index')
 router.use('/login',login)
 router.use('/register',register)
-
+router.use('/search',search)
 module.exports = router;
---
+++ router/search/index.js
@@ -0,0 +1,38 @@
+var express = require('express')
+var mysql = require('mysql')
+var app = express()
+var router = express.Router();
+var path = require('path')
+var connection = mysql.createConnection({
+  host : 'localhost',
+  port : '3306',
+  user : 'root',
+  password : 'dlwjdwo95!',
+  database : 'sogongdb'
+})
+
+
+connection.connect();
+router.post('/',function(req, res){
+  var body = req.body;
+  var id = body.ID;
+  var title = body.title;
+  var artist = body.artist;
+  var date = body.date;
+  var image = body.image;
+  var lyric = body.lyric;
+
+  console.log(body);
+  var sql = {"ID":id,"title":title,"artist":artist,"date":date,"image",image,"lyric",lyric};
+  var registerquery = connection.query('insert into music set ?',sql, function(err,rows){
+    if(err){
+      console.log(err)
+      var msg = {"status": "ERROR"}
+      res.json(msg)
+      return;
+    }
+    var msg = {"status":"OK"}
+    res.json(msg)
+  })
+})
+module.exports = router;
