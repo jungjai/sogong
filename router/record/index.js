1d480a37a1bfe72c2903160fcb94675003554318
jeong <950222@naver.com>
2018-08-27 00:21:27 +0900

    fifth_4

--- router/record/index.js
+++ router/record/index.js
@@ -3,6 +3,9 @@ var mysql = require('mysql')
 var app = express()
 var router = express.Router();
 var path = require('path')
+var multer  = require('multer')
+var upload = multer({ dest: 'uploads/' })
+
 var connection = mysql.createConnection({
   host : 'localhost',
   port : '3306',
@@ -11,14 +14,36 @@ var connection = mysql.createConnection({
   database : 'sogongdb'
 })
 connection.connect();
-router.post('/',function(req, res){
+router.post('/', upload.single('record'), function(req, res){
+  console.log(req.file);
+});
+/*
   var body = req.body;
   var id = body.ID;
-  var record = body.record;
+//  var record = body.record;
   var date = body.date;
   var name = body.name;
 
+  app.use(multer({
+    onFileUp
+  }))
   console.log(body);
+  var PythonShell = require('python-shell');
+
+  var options={
+    mode : 'json',
+    pythonPath : '',
+    pathonOptions : ['-u'],
+    scriptPath : ''.
+    args : [record]
+  };
+
+  PythonShell.run('.py', options, function (err, results) {
+    if(err) throw err;
+
+    console.log('results : %s', results);
+  });
+  /*
   var sql = {"ID":id,"record":record,"date":date,"name":name}
   var registerquery = connection.query('insert into record set ?',sql, function(err,rows){
     if(err){
@@ -29,6 +54,6 @@ router.post('/',function(req, res){
     }
     var msg = {"status":"OK"}
     res.json(msg)
-  })
-})
+
+})*/
 module.exports = router;
